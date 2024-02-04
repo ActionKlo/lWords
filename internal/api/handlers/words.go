77ff0090505106831c27adc0e3339afa2bd58c25
Andrei <actionclaw@gmail.com>
2024-02-04 13:48:37 +0100

    add method to get words list

--- internal/api/handlers/words.go
+++ internal/api/handlers/words.go
@@ -1,8 +1,10 @@
 package handlers
 
 import (
+	"github.com/labstack/echo/v4"
 	"go.uber.org/zap"
 	"lWords/internal/database/mongo"
+	"net/http"
 )
 
 type Config struct {
@@ -15,4 +17,14 @@ func NewHandlerConfig(mongo *mongo.DBService, logger *zap.Logger) *Config {
 		mongo:  mongo,
 		logger: logger,
 	}
+}
+
+func (c *Config) GetAllWords(ctx echo.Context) error {
+	words, err := c.mongo.GetWordsList()
+	if err != nil {
+		c.logger.Error("failed to get words list", zap.Error(err))
+		return ctx.JSON(http.StatusBadRequest, err)
+	}
+
+	return ctx.JSON(http.StatusOK, words)
 }
