09a3318b8ab036daefdac18b952330d04c7fa08a
Andrei <actionclaw@gmail.com>
2024-02-04 13:52:06 +0100

    add method to get words list from mongo

--- internal/database/mongo/words.go
+++ internal/database/mongo/words.go
@@ -2,51 +2,29 @@ package mongo
 
 import (
 	"context"
-	"fmt"
 	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.uber.org/zap"
 	"lWords/internal/models"
 )
 
-func (m MongoDBService) FindWords(word string) []models.Words {
-	coll := m.client.Database(m.cfg.DBName).Collection("words")
+const (
+	collectionName = "words"
+)
 
-	filter := bson.D{{"$text", bson.D{{"$search", word}}}}
+func (m DBService) GetWordsList() ([]models.Words, error) {
+	coll := m.client.Database(m.cfg.DBName).Collection(collectionName)
 
-	cursor, err := coll.Find(context.Background(), filter)
+	cursor, err := coll.Find(context.TODO(), bson.D{})
 	if err != nil {
-		m.log.Error("failed to find words", zap.Error(err))
+		m.log.Error("failed to what??")
+		return nil, err
 	}
 
-	var result []models.Words
-	if err = cursor.All(context.Background(), &result); err != nil {
+	var results []models.Words
+	if err = cursor.All(context.TODO(), &results); err != nil {
 		m.log.Error("failed to cursor words", zap.Error(err))
+		return nil, err
 	}
 
-	return result
-}
-
-func (m MongoDBService) FindWordByID() []models.Words {
-	coll := m.client.Database(m.cfg.DBName).Collection("words")
-
-	objID, err := primitive.ObjectIDFromHex("65abb6fe0db988e11c8a6736")
-	if err != nil {
-		m.log.Error("failed to create ObjectID from hex", zap.Error(err))
-	}
-	fmt.Println(objID)
-
-	filter := bson.D{{"_id", objID}}
-
-	cursor, err := coll.Find(context.Background(), filter)
-	if err != nil {
-		m.log.Error("failed to find word by ID", zap.Error(err))
-	}
-
-	var result []models.Words
-	if err = cursor.All(context.Background(), &result); err != nil {
-		m.log.Error("failed to cursor word", zap.Error(err))
-	}
-
-	return result
+	return results, nil
 }
